fafc6b68cab2db7c9d37be24f96dda76a544fa78
franci <francirizai2002@gmail.com>
2024-02-01 14:54:15 +0200

    hi

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 require 'vendor/autoload.php'; // Adjust the path based on your project structure
-
+include('index.html');
 use RedBeanPHP\R;
 
 // Set up database connection
@@ -22,7 +22,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Check if the user exists and the password is correct
     if ($user && password_verify($password, $user->password)) {
         // Authentication successful, redirect to a secure page
-        echo "<script src='code.js' ></script>";
+        echo "<script >
+        document.querySelector('.ptemp').innerHTML='hello';
+        </script>";
         header('Location: index.html');
         exit();
     } else {
