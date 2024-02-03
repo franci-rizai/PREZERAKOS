c91e6a833bd9c9a1d9f1d4fd16d0107644bbbf07
Suraj <ice20390277@uniwa.gr>
2024-02-03 15:57:28 +0200

    js fix

--- show_appointment.php
+++ show_appointment.php
@@ -2,7 +2,6 @@
 require 'vendor/autoload.php';
 use RedBeanPHP\R;
 
-// Set up database connection
 R::setup('mysql:host=sql11.freemysqlhosting.net;dbname=sql11681307', 'sql11681307', 'Y8LhenD1Xp');
 
 session_start();
@@ -28,8 +27,15 @@ if (isset($_SESSION['username'], $_SESSION['userFullName'])) {
     if ($currentUser) {
         // Get the user's appointments
         $userAppointments = getUserAppointments($_SESSION['userFullName']);
+
+        // Return user appointments as JSON
+        header('Content-Type: application/json');
+        echo json_encode($userAppointments);
+        exit();
     }
 }
 
-// Use $userAppointments as needed
+// If not logged in or user not found
+echo json_encode(['success' => false, 'message' => 'User not found']);
+exit();
 ?>
