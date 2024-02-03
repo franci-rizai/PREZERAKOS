c2347c72b39e5a4331b7b26429c6eda5c72d8ed9
Suraj <ice20390277@uniwa.gr>
2024-02-03 15:36:24 +0200

    show fix

--- show_appointment.php
+++ show_appointment.php
@@ -15,19 +15,22 @@ function getCurrentUser() {
     return null;
 }
 
-function getUserAppointments($userId) {
-    return R::findAll('appointments', 'user_id = ?', [$userId]);
+function getUserAppointments($userFullName) {
+    // Get the user's appointments based on the user's full name
+    return R::findAll('appointments', 'name = ?', [$userFullName]);
 }
 
 // Check if the user is logged in
-if (isset($_SESSION['username'])) {
+if (isset($_SESSION['username'], $_SESSION['userFullName'])) {
     // Get the logged-in user
     $currentUser = getCurrentUser();
 
     if ($currentUser) {
         // Get the user's appointments
-        $userAppointments = getUserAppointments($currentUser->id);
+        $userAppointments = getUserAppointments($_SESSION['userFullName']);
     }
+    echo '<script> console.log(' . json_encode($userAppointments) . ');</script>';
 }
 
+// Use $userAppointments as needed
 ?>
